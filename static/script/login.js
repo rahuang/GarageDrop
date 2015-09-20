80960d227ecc1292f7b993108b14a9bf94454cdf
blimper <bjli@andrew.cmu.edu>
2015-09-20 05:18:45 -0400

    small change to logo

--- static/script/login.js
+++ static/script/login.js
@@ -1,8 +1,7 @@
 var main = function(){
-	$('#wrapper').css('background-image', 'url(../static/img/garage_sale_login_open.gif)');
+	$('#house').attr('src', '../static/img/garage_sale_login_open.gif?' + Math.random());
 	setTimeout(function open(){
-		$('#wrapper').addClass("garage_opened");
-		$('#wrapper').css('background-image', 'url(../static/img/garage_sale_login_open.png)');
+		$('#house').attr('src', '../static/img/garage_sale_login_open.png');
 		/*$('#ball').attr("src", "../static/img/ball_bounce.gif?" + Math.random());*/
 		/*setTimeout(function bounce_stop(){
 			$('#ball').attr("src", "../static/img/ball_bounce_stop.png");
@@ -10,13 +9,26 @@ var main = function(){
 	}, 1000);
 
 	$('.login_btn').click(function(){
-		$('#wrapper').css('background-image', 'url(../static/img/garage_sale_login_close.gif)');
+		$('#house').attr('src', '../static/img/garage_sale_login_close.gif?' + Math.random());
 		setTimeout(function login(){
-			$('#wrapper').css('background-image', 'url(../static/img/garage_sale_login.png)');
+			$('#house').attr('src', '../static/img/garage_sale_login.png');
 			$('#wrapper').animate({'top' : '650px'}, {duration : 650});
 			$('#clouds').animate({'top' : '0px'}, {duration : 650});
 			$('.pin').animate({'top' : '50px'}, {duration : 1000});
 		}, 1000);
+	});
+
+	$('.pin').click(function(){
+		
+		$('#wrapper').animate({'top' : '0px'}, {duration : 1000});
+		$('#clouds').animate({'top' : '-800px'}, {duration : 1000});
+		$('.pin').animate({'top' : '-1500px'}, {duration : 650});
+		setTimeout(function login(){
+			$('#house').attr('src', '../static/img/garage_sale_login_open.gif?' + Math.random());
+		}, 1000);
+		setTimeout(function open2(){
+				$('#house').attr('src', '../static/img/garage_sale_login_open.png');
+		}, 2000);
 	});
 
 }

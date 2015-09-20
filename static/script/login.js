71d72fe4f3d5445f7169106a4700c79727c70776
blimper <bjli@andrew.cmu.edu>
2015-09-19 23:45:05 -0400

    changed login page

--- static/script/login.js
+++ static/script/login.js
@@ -1,8 +1,19 @@
 var main = function(){
-	$('#wrapper').addClass("garage_closed");
+	$('#wrapper').css('background-image', 'url(../static/img/garage_sale_login_open.gif)')
+			setTimeout(function close(){
+				$('#wrapper').addClass("garage_opened");
+				$('#wrapper').css('background-image', 'url(../static/img/garage_sale_login_open.png)')
+			}, 2000);
+	setTimeout(function bounce(){
+		$('.ball_bounce').css('background-image', 'url(../static/img/ball_bounce.gif)')
+		$('#wrapper').css('background-image', 'url(../static/img/garage_sale_login_open2.png)')
+		setTimeout(function bounce_stop(){
+			$('.ball_bounce').css('background-image', 'url(../static/img/ball_bounce_stop.png)')
+		}, 1000)
+	}, 3000);
 	$('.form-group').click(function(){
 		if ($('#wrapper').hasClass("garage_closed")){
-			$('#wrapper').css('background-image', 'url(../static/img/garage_sale_login.gif)')
+			$('#wrapper').css('background-image', 'url(../static/img/garage_sale_login_open.gif)')
 			$('#wrapper').removeClass("garage_closed");
 			setTimeout(function close(){
 				$('#wrapper').addClass("garage_opened");

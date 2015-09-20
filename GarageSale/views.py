dc3379f76f5b0fc8a79debf13c1a933f77e6e468
rahuang <rahuang@andrew.cmu.edu>
2015-09-20 01:18:35 -0400

    added add to cart feature

--- GarageSale/views.py
+++ GarageSale/views.py
@@ -111,7 +111,8 @@ class GaragePage(TemplateView):
 
 class MyCartPage(TemplateView):
     """ The Orders Page. """
-    def get(self, request):
+
+    def common(self, request):
         test_key = '489913f8-8da9-431b-b2d3-05b013c87077'
         test_id = 'cus_KUqEcMmgrhGHH-'
         api = pm.PostmatesAPI(test_key, test_id)
@@ -146,6 +147,24 @@ class MyCartPage(TemplateView):
         #return HttpResponse(str(cost) + " " +str(total))
 
         return render(request, 'mycart.html', {"item_costs" : item_costs, "total" : total})
+
+    def get(self, request):
+        return self.common(request)
+        
+    def post(self, request):
+        get_params = request.POST
+        if "item" in get_params:
+            connection = httplib.HTTPSConnection('api.parse.com', 443)
+            connection.connect()
+            connection.request('PUT', '/1/classes/Items/' + get_params["item"], json.dumps({
+                   "status": "IN TRANSIT"
+                 }), {
+                   "X-Parse-Application-Id": "GEhB6O9S9sJwKWRVlfcm2zghfmpN7ZIg5guhjHha",
+                   "X-Parse-REST-API-Key": "Ui7OtToUquSRwLGGHxDCLB0nX9t5o2IOwSVyRjRI",
+                   "Content-Type": "application/json"
+                 })
+
+        return self.common(request)
 
 class OrdersPage(TemplateView):
     """ The Orders Page. """
